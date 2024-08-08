8caddee99e70aa1a522edfdcf2a625148d437714
marcos <marcos@yesh.com.br>
2024-08-08 14:53:00 -0300

    ajustes modelo

--- service/models/order.js
+++ service/models/order.js
@@ -1,28 +1,32 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
 
-const Order = sequelize.define('Order', {
-    clientId: {
-        type: DataTypes.STRING,
-    },
-    projectId: {
-        type: DataTypes.STRING,
-    },
-    status: {
-        type: DataTypes.STRING,
-    },
-    creationDate: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-    },
-    isActive: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-    },
-    isDeleted: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-    },
+const Order = sequelize.define("Order", {
+  clientId: {
+    type: DataTypes.STRING,
+  },
+  projectId: {
+    type: DataTypes.STRING,
+  },
+  status: {
+    type: DataTypes.STRING,
+  },
+  creationDate: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+  isDeleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  items: {
+    type: DataTypes.JSON, // Use DataTypes.JSON para SQLite
+    allowNull: true, // Permite que o valor seja nulo
+  },
 });
 
 export default Order;

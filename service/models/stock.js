72c599830c35413e3ce1b22d86d3b638ee880135
marcos <marcos@yesh.com.br>
2024-08-08 14:53:37 -0300

    ajustes modelo

--- service/models/stock.js
+++ service/models/stock.js
@@ -1,26 +1,29 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
 
-const Stock = sequelize.define('Stock', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    address: {
-        type: DataTypes.STRING,
-    },
-    hasAddress: {
-        type: DataTypes.BOOLEAN,
-    },
-    isActive: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-    },
-    isDeleted: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-    },
+const Stock = sequelize.define("Stock", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+  },
+  hasAddress: {
+    type: DataTypes.BOOLEAN,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+  isDeleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  itens: {
+    type: DataTypes.JSON, // Use DataTypes.JSON para SQLite
+    allowNull: true, // Permite que o valor seja nulo
+  },
 });
 
 export default Stock;
-
